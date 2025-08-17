d9a4d9fa7da7ddaf4bdca307402390588ed466c2
EnzoVarani <enzovarani23@gmail.com>
2025-08-17 02:09:25 -0300

    fix(cors): Atualiza a URL de produção na lista de origens permitidas

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const stockRoutes = require('./routes/stockRoutes');
 const app = express();
 
 const allowedOrigins = [
-  'https://financial-dashboard-seis.vercel.app', // Sua futura URL de produção
+  'https://dashboard-do-mercado-financeiro.vercel.app',
   'http://127.0.0.1:5500',
   'http://localhost:5500'
 ];
